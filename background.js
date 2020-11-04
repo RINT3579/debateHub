dd17d2215a9e3da58ff159052992152152291f21
REI TAKAHASHI <1175143@cstu.nit.ac.jp>
2020-11-04 16:04:22 +0900

    初期設定画面に自動遷移するようにしました

--- background.js
+++ background.js
@@ -2,6 +2,17 @@ let code
 let content;
 let filename;
 
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    console.log(details.reason);
+
+    chrome.runtime.openOptionsPage(function () {
+        alert("初期設定を行ってください");
+    })
+
+});
+
+
 const pr_mode = () => {
     chrome.tabs.query( {active:true, currentWindow:true}, function(tabs){
         console.log("押された");
